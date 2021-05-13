bd32716e69aa15c88c27fa16a8613fb1a919f184
Rohan Bade <baderohan759@gmail.com>
2021-05-13 20:21:06 +0545

    Octal to Binary inside dict committed

--- hello.py
+++ hello.py
@@ -15,20 +15,6 @@ class NumberSystem:
         if a in NumberSystem.octal_dict.keys():
             print(NumberSystem.octal_dict[a])
 
-def append_Bits(x,L):
-    return [x+ i for i in L]
 
-
-def generate_Bits(n,memo={}):
-    if n==0 :
-        return []
-    elif n==1:
-        return ["0","1"]
-    else:
-        memo[n] = append_Bits("0",generate_Bits(n-1,memo))+append_Bits("1",generate_Bits(n-1,memo))
-        return memo[n]
-
-octal_bits = generate_Bits(3)
-hexa_bits = generate_Bits(4)
 octalnum = NumberSystem(7)
 octalnum.convert_octal_to_binary()
