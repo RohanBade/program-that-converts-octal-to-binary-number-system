706b73482ba46c0fa5c6453c3f989f29f183ef69
Rohan Bade <baderohan759@gmail.com>
2021-05-13 20:18:12 +0545

    Binary to octal Conversion stored

--- hello.py
+++ hello.py
@@ -1,7 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 13 19:51:34 2021
+class NumberSystem:
+    octal_dict = {"0": "000",
+                  "1":"001",
+                  "2":"010",
+                  "3":"011",
+                  "4":"100",
+                  "5":"101",
+                  "6":"110",
+                  "7":"111"}
+    
+    def __init__(self,number):
+        self.number = number
+    def convert_octal_to_binary(self):
+        a= str(self.number)
+        if a in NumberSystem.octal_dict.keys():
+            print(NumberSystem.octal_dict[a])
 
-@author: DELL
-"""
+def append_Bits(x,L):
+    return [x+ i for i in L]
 
+
+def generate_Bits(n,memo={}):
+    if n==0 :
+        return []
+    elif n==1:
+        return ["0","1"]
+    else:
+        memo[n] = append_Bits("0",generate_Bits(n-1,memo))+append_Bits("1",generate_Bits(n-1,memo))
+        return memo[n]
+
+octal_bits = generate_Bits(3)
+hexa_bits = generate_Bits(4)
+octalnum = NumberSystem(7)
+octalnum.convert_octal_to_binary()
